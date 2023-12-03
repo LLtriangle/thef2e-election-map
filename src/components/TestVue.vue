d5718867e8a51c0838472c40dcbc71b492fe3c97
LAPTOP-ATM7TCEI\user <laleole299@gmail.com>
2023-12-03 22:24:56 +0800

    feat: add vue

---
+++ src/components/TestVue.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <input v-model="count" />
+        <p :class="color">{{ count }}</p>
+        <p>{{ color }}</p>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+const count = ref(0);
+const color = ref("skyblue");
+</script>
+
+<style>
+.skyblue {
+    color: skyblue;
+}
+</style>
