599c805c90a46b1c99d6249deba9d074fd3db5bb
LAPTOP-ATM7TCEI\user <laleole299@gmail.com>
2023-12-03 23:49:45 +0800

    feat: add eslint and prettier

--- src/components/TestVue.vue
+++ src/components/TestVue.vue
@@ -1,20 +1,20 @@
 <template>
-    <div :class="tailWindStyle">
-        <input v-model="count" />
-        <p :class="color">{{ count }}</p>
-        <p>{{ color }}</p>
-    </div>
+  <div :class="tailWindStyle">
+    <input v-model="count" />
+    <p :class="color">{{ count }}</p>
+    <p>{{ color }}</p>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref } from 'vue';
 const count = ref(0);
-const color = ref("skyblue");
-const tailWindStyle = ref("bg-gradient-to-r from-purple-500 to-pink-500");
+const color = ref('skyblue');
+const tailWindStyle = ref('bg-gradient-to-r from-purple-500 to-pink-500');
 </script>
 
 <style>
 .skyblue {
-    color: skyblue;
+  color: skyblue;
 }
 </style>
