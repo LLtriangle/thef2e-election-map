6f1c13a0afec7e7624d0d9ba79da4b64b0087518
LAPTOP-ATM7TCEI\user <laleole299@gmail.com>
2023-12-15 10:39:15 +0800

    feat: add footer

---
+++ src/components/Layout/Footer.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+const props = defineProps(['footerGoMap']);
+</script>
+
+<template>
+  <footer class="bottom-0 w-full bg-black px-8 py-3 text-white md:flex md:justify-between">
+    <a
+      class="goToPoll group mb-4 flex text-base md:mb-0 md:w-[80%] md:max-w-[300px] md:text-xl"
+      :class="{
+        block: footerGoMap,
+        hidden: !footerGoMap,
+      }"
+      href=""
+    >
+      <span class="mr-2 mt-auto block">看選前民調</span>
+      <span
+        class="after:translzte-x-2 relative mb-1 block w-[100px] border-b-[2px] border-b-[#DD4D2A] transition-all after:absolute after:bottom-[3px] after:right-[-1px] after:w-[20px] after:rotate-[30deg] after:border-b-[2px] after:border-b-[#DD4D2A] group-hover:w-[130px] group-hover:translate-x-2 group-hover:translate-y-[-6px] group-hover:border-b-[1px] after:group-hover:w-[15px] after:group-hover:border-b-[1px] min-[280px]:group-hover:w-[120px] sm:w-[150px] sm:after:bottom-[6px] sm:after:w-[30px] sm:group-hover:w-[180px] sm:after:group-hover:bottom-[4px] sm:after:group-hover:w-[20px]"
+      ></span>
+
+      <!-- <img class="max-w-[126px]" src="../../assets/images/footer/arrow.svg" alt="" /> -->
+    </a>
+    <div class="credit min-[860px]:flex">
+      <a class="contact mb-2 block text-xs sm:mr-7 md:my-auto md:text-base" href="">樹懶設計 shulian@gmail.com</a>
+      <div class="copyright text-xs md:my-auto md:text-base">© 2023 樹懶設計 shulian 版權所有</div>
+    </div>
+  </footer>
+</template>
+
+<style></style>
