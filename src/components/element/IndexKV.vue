fec6e9aec09620308da40127a6b42ad26c5b4356
LAPTOP-ATM7TCEI\user <laleole299@gmail.com>
2023-12-29 11:19:53 +0800

    feat: change variable name in indexKV

--- src/components/element/IndexKV.vue
+++ src/components/element/IndexKV.vue
@@ -1,32 +1,29 @@
 <script setup lang="ts">
 import { ref, onMounted, reactive, toRefs } from 'vue';
 
-const { candidateKV } = toRefs(reactive({ candidateKV: null }));
-const bubbleHeight = ref(0);
+const { candidateKVTsai } = toRefs(reactive({ candidateKVTsai: null }));
+const speechBubbleHeight = ref(0);
 
-const calculateBubbleHeight = () => {
-  const candidateKVElement = candidateKV.value;
-  console.log(candidateKVElement);
+const calculateSpeechBubbleHeight = () => {
+  const candidateKVTsaiElement = candidateKVTsai.value;
 
-  if (candidateKVElement) {
-    bubbleHeight.value = candidateKVElement.clientHeight;
-    console.log(bubbleHeight.value);
+  if (candidateKVTsaiElement) {
+    speechBubbleHeight.value = candidateKVTsaiElement.clientHeight;
   }
 };
 
 onMounted(() => {
-  calculateBubbleHeight();
-  window.addEventListener('resize', calculateBubbleHeight);
+  calculateSpeechBubbleHeight();
+  window.addEventListener('resize', calculateSpeechBubbleHeight);
 });
 </script>
 
 <template>
-  <!-- <div class="h-[40vh]"></div> -->
-  <section class="indexKv h-[30vh] overflow-x-clip overflow-y-visible md:h-[80vh]">
+  <section class="indexKv overflow-x-clip overflow-y-visible">
     <div class="candidatesContainer m-auto flex h-full max-w-[1000px] md:w-[60%]">
       <div class="candidateContainer w-full">
         <div class="candidate relative h-full w-full">
-          <a href="https://www.google.com.tw/" class="group">
+          <a href="" class="group">
             <img
               class="candidateKV han absolute bottom-[5%] left-[-8%] w-[150%] max-w-[400px] group-hover:bottom-[8%] 2xl:left-[25%]"
               src="../../assets/images/index/kv-han.svg"
@@ -42,9 +39,9 @@ onMounted(() => {
       </div>
       <div class="candidateContainer w-full">
         <div class="candidate relative h-full w-full">
-          <a href="https://www.google.com.tw/" class="group">
+          <a href="" class="group">
             <img
-              ref="candidateKV"
+              ref="candidateKVTsai"
               class="candidateKV tsai absolute bottom-[-3%] left-[-25%] z-30 w-[150%] max-w-[400px] group-hover:bottom-0 2xl:left-0"
               src="../../assets/images/index/kv-tsai.svg"
               alt=""
@@ -52,7 +49,7 @@ onMounted(() => {
             <img
               class="cta tsai absolute bottom-[350px] left-[-5%] w-full max-w-[280px] drop-shadow-xl group-hover:rotate-6 md:bottom-[350px] lg:bottom-[450px] 2xl:left-[20%]"
               src="../../assets/images/index/cta-tsai.svg"
-              :style="{ bottom: `${bubbleHeight}px` }"
+              :style="{ bottom: `${speechBubbleHeight}px` }"
               alt=""
             />
           </a>
@@ -60,7 +57,7 @@ onMounted(() => {
       </div>
       <div class="candidateContainer w-full">
         <div class="candidate relative h-full w-full">
-          <a href="https://www.google.com.tw/" class="group">
+          <a href="" class="group">
             <img
               class="candidateKV song absolute bottom-[5%] left-[-45%] w-[160%] max-w-[420px] group-hover:rotate-[-6deg]"
               src="../../assets/images/index/kv-song.svg"
@@ -72,7 +69,6 @@ onMounted(() => {
               alt=""
             />
           </a>
-          <!-- <p><span class="text-[#DD4D2A]">了解</span> 韓國瑜</p> -->
         </div>
       </div>
     </div>
